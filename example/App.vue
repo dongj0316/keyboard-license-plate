7cc96ea9a0a0d4de7fde503f68f7c8c32b8e11e0
dongj0316 <475586651@qq.com>
2019-04-23 16:00:43 +0800

    refactor: 修改目录名称 style: 修改组件样式

---
+++ example/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <div class="cell">
+      <div class="label">车牌号码</div>
+      <div class="field">
+        <keyboard-license-plate v-model="platenumber"></keyboard-license-plate>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import KeyboardLicensePlate from '../keyboard-license-plate/index.js'
+
+export default {
+  name: 'app',
+  components: {
+    KeyboardLicensePlate
+  },
+  data () {
+    return {
+      platenumber: ''
+    }
+  }
+}
+</script>
+
+<style lang="less">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+.cell {
+  display: flex;
+  padding: 10px 15px;
+  .label {
+    min-width: 90px;
+  }
+  .field {
+    flex: 1;
+  }
+}
+</style>
